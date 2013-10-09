c82cbe1e1867e6c666d3e4ee17fd1fcd0d818282
jybox <m@jybox.net>
2013-10-09 11:46:23 +0800

    Hello World

---
+++ array+/array.h
@@ -0,0 +1,9 @@
+#ifndef H_JYDS_ARRAY_PLUS
+#define H_JYDS_ARRAY_PLUS
+
+class Array
+{
+
+};
+
+#endif // H_JYDS_ARRAY_PLUS
---
+++ array+/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "array.h"
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World"<<endl;
+}
