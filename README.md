c59884b6d0e569d818f3e264004a8b43fc6bd3f2
jybox <m@jybox.net>
2013-10-09 15:17:22 +0800

    array+, non-finish

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-jyDS
-====
+## jyDS
+Data Struct by C/C++.
+
+* array+ Fixed Array by C++
--- array+/array.h
+++ array+/array.h
@@ -1,9 +1,99 @@
 #ifndef H_JYDS_ARRAY_PLUS
 #define H_JYDS_ARRAY_PLUS
 
-class Array
+template<class T> class Array
+{
+public:
+    Array(int length);
+    Array(int length, const T defaultValue);
+    Array(const Array &other);
+    ~Array();
+    Array<T>& operator = (const Array<T> &other);
+
+    T& at(int offset);
+    T& operator [] (int offset);
+    T& frist();
+    T& last();
+    T* raw();
+
+    int size() const;
+
+private:
+    T* data;
+    int length;
+};
+
+template<class T> Array<T>::Array(int length)
+{
+    data = new T[length];
+    this->length = length;
+}
+
+template<class T> Array<T>::Array(int length, const T defaultValue)
+{
+    data = new T[length];
+    this->length = length;
+    for(int *p = data; p != data[length]; p++)
+        *p = defaultValue;
+}
+
+template<class T> Array<T>::Array(const Array &other)
+{
+    length = other.length;
+    data = new T[length];
+
+    for(int i = 0; i < length; i++)
+        data[i] = other.data[i];
+}
+
+template<class T> Array<T>::~Array()
+{
+    delete data;
+}
+
+template<class T> Array<T>& Array<T>::operator = (const Array& other)
+{
+    if(&other == this)
+        return *this;
+
+    delete data;
+
+    length = other.length;
+    data = new T[length];
+
+    for(int i = 0; i < length; i++)
+        data[i] = other.data[i];
+}
+
+
+template<class T> T& Array<T>::at(int offset)
 {
 
-};
+}
+
+template<class T> T& Array<T>::operator [] (int offset)
+{
+
+}
+
+template<class T> T& Array<T>::frist()
+{
+
+}
+
+template<class T> T& Array<T>::last()
+{
+
+}
+
+template<class T> T* Array<T>::raw()
+{
+
+}
+
+template<class T> int Array<T>::size() const
+{
+
+}
 
 #endif // H_JYDS_ARRAY_PLUS
