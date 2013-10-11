24b86e405c624904260e9f96c378783fe7945fc0
jybox <m@jybox.net>
2013-10-11 11:14:30 +0800

    Change param order

--- array/array.h
+++ array/array.h
@@ -10,7 +10,7 @@ typedef struct {
 Array* array_create(int elemSize, int length);
 Array* array_createWithDefaultValue(int elemSize, int length, void *defaultValue);
 Array* array_copy(Array *other);
-void array_override(Array *source, Array *target);
+void array_override(Array *target, Array *source);
 void array_free(Array *array);
 
 void* array_at(Array *array, int offset);
