3c7247cf6d17202cf400ef61aede5ba38a9e2037
jybox <m@jybox.net>
2013-11-05 10:10:04 +0800

    update array.h

--- array/array.h
+++ array/array.h
@@ -1,27 +1,35 @@
 #ifndef H_JYDS_ARRAY
 #define H_JYDS_ARRAY
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct {
     int elemSize;
     int length;
     void *data;
-} Array;
+} sArray;
 
-Array* array_create(int elemSize, int length);
-Array* array_create_defaults(int elemSize, int length, void *defaultValue);
-Array* array_copy(Array *other);
-void array_override(Array *target, Array *source);
-void array_free(Array *array);
+sArray* array_create(int elemSize, int length);
+sArray* array_create_defaults(int elemSize, int length, void *defaultValue);
+sArray* array_copy(sArray *other);
+void array_override(sArray *target, sArray *source);
+void array_free(sArray *array);
 
-void* array_at(Array *array, int offset);
-void* array_frist(Array *array);
-void* array_last(Array *array);
-void* array_raw(Array *array);
+void* array_at(sArray *array, int offset);
+void* array_frist(sArray *array);
+void* array_last(sArray *array);
+void* array_raw(sArray *array);
 
-int array_size(Array *array);
-int array_elemSize(Array *array);
+int array_size(sArray *array);
+int array_elemSize(sArray *array);
 
-int array_equal(Array *a, Array *b);
-int array_not_equal(Array *a, Array *b);
+int array_equal(sArray *a, sArray *b);
+int array_not_equal(sArray *a, sArray *b);
+
+#ifdef __cplusplus
+}	// extern "C" {
+#endif
 
 #endif // H_JYDS_ARRAY
